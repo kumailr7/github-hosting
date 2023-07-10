43262558ea117ced079d03770404421e611fc7eb
kumailrizvi7 <kumailrizvi70@gmail.com>
2023-07-11 00:37:11 +0530

    Added Docker file

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello, Kumail Rizvi!"
+    return 'Hello to Docker Devops Guys'
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
