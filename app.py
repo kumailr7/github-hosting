734ee2316f6dd8e47ece2db36df53776aca3fe79
kumailrizvi7 <kumailrizvi70@gmail.com>
2023-07-11 00:48:19 +0530

    Updated the files

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello to Docker Devops Guys'
+    return 'Hello to Docker Devops Guys From Blenhiem Chalcot'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
